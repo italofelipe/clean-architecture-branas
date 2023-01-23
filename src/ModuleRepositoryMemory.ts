de10433c31d2abde938fc3c590db8cb5f3016742
italofelipe <felipe.italo@hotmail.com>
2023-01-23 08:28:43 -0300

    feat: module separated into its own class

--- src/ModuleRepositoryMemory.ts
+++ src/ModuleRepositoryMemory.ts
@@ -1,101 +1,100 @@
+import Module from "./Module";
 import ModuleRepository from "./ModuleRepository";
 
 export default class ModuleRepositoryMemory implements ModuleRepository {
-	modules: any[];
+	modules: Module[];
 
-	constructor() {
+	constructor () {
 		this.modules = [
-			{
+			new Module({
 				level: "EF1",
 				code: "1",
-				description: "1o ano",
+				description: "1o Ano",
 				minimumAge: 6,
 				price: 15000
-			},
-			{
+			}),
+			new Module({
 				level: "EF1",
 				code: "2",
-				description: "2o ano",
+				description: "2o Ano",
 				minimumAge: 7,
 				price: 15000
-			},
-			{
+			}),
+			new Module({
 				level: "EF1",
 				code: "3",
-				description: "3o ano",
+				description: "3o Ano",
 				minimumAge: 8,
 				price: 15000
-			},
-			{
+			}),
+			new Module({
 				level: "EF1",
 				code: "4",
-				description: "4o ano",
+				description: "4o Ano",
 				minimumAge: 9,
 				price: 15000
-			},
-			{
+			}),
+			new Module({
 				level: "EF1",
 				code: "5",
-				description: "5o ano",
+				description: "5o Ano",
 				minimumAge: 10,
 				price: 15000
-			},
-			{
+			}),
+			new Module({
 				level: "EF2",
 				code: "6",
-				description: "6o ano",
+				description: "6o Ano",
 				minimumAge: 11,
-				price: 15000
-			},
-			{
+				price: 14000
+			}),
+			new Module({
 				level: "EF2",
 				code: "7",
-				description: "7o ano",
+				description: "7o Ano",
 				minimumAge: 12,
-				price: 15000
-			},
-			{
+				price: 14000
+			}),
+			new Module({
 				level: "EF2",
 				code: "8",
-				description: "8o ano",
+				description: "8o Ano",
 				minimumAge: 13,
-				price: 15000
-			},
-			{
+				price: 14000
+			}),
+			new Module({
 				level: "EF2",
 				code: "9",
-				description: "9o ano",
+				description: "9o Ano",
 				minimumAge: 14,
-				price: 15000
-			},
-			{
+				price: 14000
+			}),
+			new Module({
 				level: "EM",
 				code: "1",
-				description: "1o ano",
+				description: "1o Ano",
 				minimumAge: 15,
 				price: 17000
-			},
-			{
+			}),
+			new Module({
 				level: "EM",
 				code: "2",
-				description: "2o ano",
+				description: "2o Ano",
 				minimumAge: 16,
 				price: 17000
-			},
-			{
+			}),
+			new Module({
 				level: "EM",
 				code: "3",
-				description: "3o ano",
+				description: "3o Ano",
 				minimumAge: 17,
 				price: 17000
-			}
+			})
 		];
 	}
 
 	findByCode(level: string, code: string) {
-		const module = this.modules.find(
-			(module: any) => module.level === level && module.code === code
-		);
+		const module = this.modules.find(module => module.level === level && module.code === code);
 		if (!module) throw new Error("Module not found");
 		return module;
 	}
