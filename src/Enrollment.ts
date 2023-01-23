7ad3736034e3e75be13060ac28c0bc1421798308
italofelipe <felipe.italo@hotmail.com>
2023-01-23 08:29:45 -0300

    feat: enrollment core logic split among different files

--- src/Enrollment.ts
+++ src/Enrollment.ts
@@ -1,23 +1,75 @@
 import Student from "./Student";
+import Level from "./Level";
+import Module from "./Module";
+import Classroom from "./Classroom";
+import EnrollmentCode from "./EnrollmentCode";
+import Invoice from "./Invoice";
 
 export default class Enrollment {
 	student: Student;
-	level: string;
-	module: string;
-	clazz: string;
-	code: string;
+	level: Level;
+	module: Module;
+	classRoom: Classroom;
+	code: EnrollmentCode;
+	issueDate: Date;
+	sequence: number;
+	installments: number;
+	invoices: Invoice[];
 
 	constructor(
 		student: Student,
-		level: string,
-		module: string,
-		clazz: string,
-		code: string
+		level: Level,
+		module: Module,
+		classRoom: Classroom,
+		issueDate: Date,
+		sequence: number,
+		installments: number = 12
 	) {
+		if (student.getAge() < module.minimumAge)
+			throw new Error("Student below minimum age");
+		if (classRoom.isFinished(issueDate)) {
+			throw new Error("Class is already finished");
+		}
+		if (classRoom.getProgress(issueDate) > 25) {
+			throw new Error("Class is already started");
+		}
 		this.student = student;
 		this.level = level;
 		this.module = module;
-		this.clazz = clazz;
-		this.code = code;
+		this.classRoom = classRoom;
+		this.sequence = sequence;
+		this.issueDate = issueDate;
+
+		this.code = new EnrollmentCode(
+			level.code,
+			module.code,
+			classRoom.code,
+			issueDate,
+			sequence
+		);
+		this.invoices = [];
+		this.installments = installments;
+		this.generateInvoices();
+	}
+	generateInvoices() {
+		let installmentAmount =
+			Math.trunc((this.module.price / this.installments) * 100) / 100;
+		for (let i = 1; i <= this.installments; i++) {
+			this.invoices.push(
+				new Invoice(
+					this.code.value,
+					i,
+					this.issueDate.getFullYear(),
+					installmentAmount
+				)
+			);
+		}
+		const total = this.invoices.reduce((total, invoice) => {
+			total += invoice.amount;
+			return total;
+		}, 0);
+
+		const rest = Math.trunc((this.module.price - total) * 100) / 100;
+		this.invoices[this.installments - 1].amount = installmentAmount + rest;
 	}
 }
--- src/EnrollmentRepositoryMemory.ts
+++ src/EnrollmentRepositoryMemory.ts
@@ -1,35 +1,23 @@
-import EnrollmentRepository from "./EnrollmentRepository";
 import Enrollment from "./Enrollment";
+import EnrollmentRepository from "./EnrollmentRepository";
 
-export default class EnrollmentRepositoryMemory
-implements EnrollmentRepository
-{
+export default class EnrollmentRepositoryMemory implements EnrollmentRepository {
 	enrollments: Enrollment[];
 
-	constructor() {
+	constructor () {
 		this.enrollments = [];
 	}
 
+	save(enrollment: Enrollment): void {
+		this.enrollments.push(enrollment);
+	}
+	findAllByClassroom(level: string, module: string, classroom: string) {
+		return this.enrollments.filter(enrollment => enrollment.level.code === level && enrollment.module.code === module && enrollment.classRoom.code === classroom);
+	}
+	findByCpf(cpf: string) {
+		return this.enrollments.find(enrollment => enrollment.student.cpf.value === cpf);
+	}
 	count(): number {
 		return this.enrollments.length;
-	}
-
-	findAllByClass(level: string, module: string, clazz: string): any {
-		return this.enrollments.filter(
-			(enrollment) =>
-				enrollment.level === level &&
-				enrollment.module === module &&
-				enrollment.clazz === clazz
-		);
-	}
-
-	findByCpf(cpf: string): any {
-		return this.enrollments.find(
-			(enrollment) => enrollment.student.cpf.value === cpf
-		);
-	}
-
-	save(enrollment: any): void {
-		this.enrollments.push(enrollment);
 	}
 }
