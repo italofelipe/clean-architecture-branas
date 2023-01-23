f249cbde78c55c74757dcc30fff5fef871c8b1d2
italofelipe <felipe.italo@hotmail.com>
2023-01-23 08:26:40 -0300

    feat: implemented classroom class

--- src/ClassRepositoryMemory.ts
+++ src/ClassRepositoryMemory.ts
@@ -1,22 +1,41 @@
-import ClassRepository from "./ClassRepository";
+import Classroom from "./Classroom";
+import ClassroomRepository from "./ClassroomRepository";
 
-export default class ClassRepositoryMemory implements ClassRepository {
-	classes: any[];
+export default class ClassroomRepositoryMemory implements ClassroomRepository {
+	classrooms: any[];
 
-	constructor() {
-		this.classes = [
-			{
+	constructor () {
+		this.classrooms = [
+			new Classroom({
 				level: "EM",
 				module: "3",
 				code: "A",
-				capacity: 2
-			}
+				capacity: 2,
+				startDate: new Date("2023-01-14"),
+				endDate: new Date("2023-07-29")
+			}),
+			new Classroom({
+				level: "EM",
+				module: "3",
+				code: "B",
+				capacity: 2,
+				startDate: new Date("2022-01-01"),
+				endDate: new Date("2022-01-30")
+			}),
+			new Classroom({
+				level: "EM",
+				module: "3",
+				code: "C",
+				capacity: 2,
+				startDate: new Date("2023-01-01"),
+				endDate: new Date("2023-02-20")
+			})
 		];
 	}
 
-	findByCode(code: string): any {
-		const clazz = this.classes.find((clazz: any) => clazz.code === code);
-		if (!clazz) throw new Error("Class not found");
-		return clazz;
+	findByCode(code: string) {
+		const classroom = this.classrooms.find(classroom => classroom.code === code);
+		if (!classroom) throw new Error("Classroom not found");
+		return classroom;
 	}
 }
